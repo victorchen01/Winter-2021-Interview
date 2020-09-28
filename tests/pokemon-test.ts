8fd4144165a901906afe3fb8ca47745c614ebfb8
Ivana Erlich <ierli042@uottawa.ca>
2020-09-28 14:51:04 -0400

    added endpoint test

--- tests/pokemon-test.ts
+++ tests/pokemon-test.ts
@@ -2,6 +2,10 @@ import {} from 'jasmine';
 import {Pokemon} from '../src/pokemon/pokemon'
 
 describe('Pokemon Tests', () => {
+  it('validate api is up', async () => {
+    const pokemon = new Pokemon();
+    expect(await pokemon.getPokemonByName('pikachu')).toBeTruthy();
+  });
   it('test valid name parameter', async () => {
     const pokemon = new Pokemon();
     expect(pokemon.checkNameValid('ads')).toBeTruthy();
